510ef65e046ae48b997e63cb6c681f058a75075a
Parker Catalano <catalano.work@gmail.com>
2017-12-26 18:22:01 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -70,6 +70,8 @@ class Game
     if input == "0"
       Game.new(player_1 = Players::Computer.new('X'), player_2 = Players::Computer.new('O'), board = Board.new).play
     elsif input == "1"
+      puts "Would you like to go first? Yes or No"
+      player = gets.strip
       Game.new(player_1 = Players::Human.new('X'), player_2 = Players::Computer.new('O'), board = Board.new).play
     elsif input == "2"
       Game.new.play
